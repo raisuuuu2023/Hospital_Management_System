aa7613e9379f451cec2153cb1fc63e599e9df88a
raisuuuu2023 <raisa22025000@gmail.com>
2026-06-02 20:45:23 +0600

    feat: added doctor profile and prescription routes

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,12 @@ app.use(express.json());
 const authRoutes = require('./routes/auth');
 const patientRoutes = require('./routes/patients');
 const appointmentRoutes = require('./routes/appointments');
+const doctorRoutes      = require('./routes/doctors');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/patients', patientRoutes);
 app.use('/api/appointments', appointmentRoutes);
+app.use('/api/doctors',      doctorRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hospital API is running' });
